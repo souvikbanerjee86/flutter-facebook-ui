1cd926437dfe663664b40e7cc1043af82e0a5e23
Souvik Banerjee <souvik.mlindia@gmail.com>
2021-06-19 00:25:16 +0100

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/data/data.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              "facebook",
+              style: TextStyle(
+                  letterSpacing: -1.2,
+                  color: Palette.facebookBlue,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(
+                icon: Icons.search,
+                iconSize: 30.0,
+                onPressed: () {
+                  print("Search");
+                },
+              ),
+              CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30.0,
+                onPressed: () {
+                  print("Messanger");
+                },
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: CreatePostContainer(currentUser: currentUser),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(
+              child: Rooms(onlineUsers: onlineUsers),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/rooms.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  const Rooms({Key key, @required this.onlineUsers}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      color: Colors.white,
+      child: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 10.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1 + onlineUsers.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _createRoomButton(),
+              );
+            }
+            User user = onlineUsers[index - 1];
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ProfileAvatar(
+                imageUrl: user.imageUrl,
+                isActive: true,
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class _createRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("Video Record Pressed");
+      },
+      child: Container(
+        width: 100,
+        height: 30,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          border: Border.all(
+            width: 3.0,
+            color: Colors.blueAccent[100],
+          ),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              Icons.video_call,
+              size: 35.0,
+              color: Colors.purpleAccent,
+            ),
+            const SizedBox(
+              width: 4.0,
+            ),
+            Text(
+              'Create\nRoom',
+              style: TextStyle(color: Palette.facebookBlue),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
